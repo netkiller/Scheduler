1d5035f6ebacc7aaaa6b97425675b75f21941831
neo chen <netkiller@msn.com>
2014-07-02 10:19:30 +0800

    增加命名空间

--- Scheduler.class.php
+++ Scheduler.class.php
@@ -1,5 +1,6 @@
 <?php
 class Scheduler {
+    public $cron = array();
     public function __construct() {
         //print('aaa');
     }
@@ -18,5 +19,19 @@ class Scheduler {
     public function monthly($param) {
 
     }
-
+	
+    public function join($schedule, $cmd, $status = TRUE){
+        $this->cron[] = array(($status?'':'#').$schedule => $cmd);
+    }
+    public function debug(){
+//        print_r($this->cron);
+        foreach($this->cron as $line){
+//            print_r($line);
+            list($sch, $cmd) = each($line);
+            printf("%s\t%s\n", $sch, $cmd);
+        }
+    }
+    public function setup(){
+        
+    }	
 }
--- test.php
+++ test.php
@@ -26,34 +26,16 @@ $mutex->destroy($mutex_key);
 echo $remote->get($mutex_key);
 */
 
-/*
-<<<<<<< HEAD
 $lock = new Scheduler\Lock\RemoteLock();
 $mutex = new Scheduler\Mutex\ClusterMutex($lock);
 $task = new Scheduler\Task\Task($mutex);
 $task->run();
-=======
-$lock = new RemoteLock();
-$mutex = new ClusterMutex($lock);
-$task = new Task($mutex);
->>>>>>> 90d6be8970a700f6caae20022ce68e40e6f965dd
-*/
+
 $scheduler = new Scheduler();
 $scheduler->join('*/5 * * * *', '/var/www/test.php');
 $scheduler->join('*/5 * * * *', '/var/www/abc.php', FALSE);
 $scheduler->debug();
 $scheduler->setup();
 
-<<<<<<< HEAD
-//$scheculer = new Scheduler();
-//$scheculer->run();
-
-
 //$task = new Scheduler\Task\Task(new Scheduler\Mutex\ClusterMutex(new Scheduler\Lock\RemoteLock()));
 //$task->run();
-=======
-/*
-$task = new Task(new ClusterMutex(new RemoteLock()));
-$task->run();
-*/
->>>>>>> 90d6be8970a700f6caae20022ce68e40e6f965dd
